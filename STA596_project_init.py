f1e97dd7a83f2bdd8175d3b811c01c42623d1f34
Angyalka Valcsics <36708283+angyalkavalcsics@users.noreply.github.com>
2021-11-01 21:03:37 -0400

    Update STA596_project_init.py

--- STA596_project_init.py
+++ STA596_project_init.py
@@ -21,7 +21,16 @@ adj = unpickled_df.iloc[:, 8]
 # should have 7520 edges
 first = adj[0].todense()
 G = nx.convert_matrix.from_numpy_matrix(first) 
+###############################################################################
+# Trying to narrow things down
+bact_prot = unpickled_df.loc[unpickled_df["Taxonomy Level 2"] == "Bacteria_Proteobacteria"]
+# idk? randomly sample some of these
+df_reduced = bact_prot.sample(n = 45)
+adj = df_reduced.iloc[:, 8]
 
+first = adj.iloc[44].todense()
+G = nx.convert_matrix.from_numpy_matrix(first) 
+###############################################################################
 # Playing around with different drawings
 # my vote is for kawai
 #ax.set_title('species name')
@@ -47,7 +56,7 @@ it has. The degree centrality for a node is simply its degree.
 '''
 
 centrality = nx.degree_centrality(G)
-print(['%s %0.2f'%(node,centrality[node]) for node in centrality])
+#print(['%s %0.2f'%(node,centrality[node]) for node in centrality])
 centrality_df = pd.DataFrame.from_dict(centrality, orient='index')
 avg_centrality = np.mean(centrality_df.iloc[:, 0])
 
@@ -63,7 +72,7 @@ have high scores.
 '''
 
 eig_centrality = nx.eigenvector_centrality(G)
-print(['%s %0.2f'%(node,eig_centrality[node]) for node in eig_centrality])
+# really unfortunate that its in the form of a dict
 eig_centrality_df = pd.DataFrame.from_dict(eig_centrality, orient='index')
 avg_eig_centrality = np.mean(eig_centrality_df.iloc[:, 0])
 
@@ -80,18 +89,54 @@ avg_triangles = np.mean(list(triangles.values()))
 
 # Modularity
 
-
+'''
+Networks with high modularity have dense connections between 
+the nodes within modules but sparse connections between nodes 
+in different modules. 
+'''
+import networkx.algorithms.community as nx_comm
+modularity = nx_comm.modularity(G, nx_comm.label_propagation_communities(G))
 
 
 # Run this for subset of species and store values
 
-
-
-
+res = pd.DataFrame({'Average Centrality' : [],
+                    'Average Eigencentrality' : [],
+                    'Average Closed Triangles' : [],
+                    'Modularity' : []
+                    })
+# adj defined on line 19
+for i in range(len(adj)):
+    temp = adj.iloc[i].todense()
+    G = nx.convert_matrix.from_numpy_matrix(temp) 
+    
+    centrality = nx.degree_centrality(G)
+    centrality_df = pd.DataFrame.from_dict(centrality, orient='index')
+    avg_centrality = np.mean(centrality_df.iloc[:, 0])
+    res.loc[i, 'Average Centrality'] = avg_centrality
+    
+    # really unfortunate that its in the form of a dict
+    eig_centrality = nx.eigenvector_centrality(G, max_iter=600)
+    eig_centrality_df = pd.DataFrame.from_dict(eig_centrality, orient='index')
+    avg_eig_centrality = np.mean(eig_centrality_df.iloc[:, 0])
+    res.loc[i, 'Average Eigencentrality'] = avg_eig_centrality
+    
+    triangles = nx.triangles(G)
+    avg_triangles = np.mean(list(triangles.values()))
+    res.loc[i, 'Average Closed Triangles'] = avg_triangles
+    
+    modularity = nx_comm.modularity(G, nx_comm.label_propagation_communities(G))
+    res.loc[i, 'Modularity'] = modularity
+    
+    
 # Fit a linear model
 
-
-
+import statsmodels.api as sm
+X = sm.add_constant(res)
+y = list(df_reduced['Evolution'])
+model = sm.OLS(y, X)
+est = model.fit()
+print(est.summary())
 
 
 
