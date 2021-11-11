f0fbd606908628d38795f27496495976afcc6cbd
Jesse Hautala <jesse.hautala@maine.edu>
2021-11-11 01:22:14 +0000

    added provisional clique stats; removed sleeps

--- STA596_project_init.py
+++ STA596_project_init.py
@@ -9,6 +9,7 @@ import pandas as pd
 import pickle
 import numpy as np
 import networkx as nx
+import seaborn as sns
 import matplotlib.pyplot as pyplot
 import networkx.algorithms.community as nx_comm
 from networkx.algorithms.flow import shortest_augmenting_path
@@ -111,6 +112,26 @@ modularity = nx_comm.modularity(G, nx_comm.label_propagation_communities(G))
 # node connectivity
 nx.node_connectivity(G, flow_func=shortest_augmenting_path)
 
+# Cliques
+
+class CliqueStats:
+    def __init__(self, df):
+        self.clique_count = df['count'].sum()
+        self.size_max = df['size'].max()
+        self.size_mode = df.iloc[df['count'].idxmax()]['size']
+        self.size_mean = (df['size']  * df['count']).sum() / self.clique_count
+        
+def get_clique_count_df(G):
+    clique_counts = {}
+    for clique in nx.find_cliques(G):
+        clique_size = len(clique)
+        clique_counts[clique_size] = clique_counts.get(clique_size, 0) + 1
+    
+    # convert to pandas series
+    clique_count_df = pd.DataFrame.from_dict(clique_counts,orient='index').reset_index()
+    clique_count_df.columns = ['size', 'count']
+    return clique_count_df
+
 # Run this for subset of species and store values
 
 '''
@@ -121,10 +142,19 @@ max iterations to like 1000 then just decided to leave it alone.
 Also I took out node connectivity since it is 1 for each network.
 '''
 
-res = pd.DataFrame({'Average Centrality' : [],
-                    'Average Closed Triangles' : [],
-                    'Modularity' : []
-                    })
+res = pd.DataFrame({
+    'Average Centrality': [],
+    'Average Closed Triangles': [],
+    'Modularity': [],
+    'Clique Count': [],
+    'Clique-Size Max': [], # aka "clique number"
+    'Clique-Size Mode': [],
+    'Clique-Size Mean': [],
+    'LCSG Clique Count': [],
+    'LCSG Clique-Size Max': [], # aka "clique number"
+    'LCSG Clique-Size Mode': [],
+    'LCSG Clique-Size Mean': [],
+    })
 
 for i in tqdm(range(len(adj))):
     temp = adj.iloc[i].todense()
@@ -144,18 +174,42 @@ for i in tqdm(range(len(adj))):
     modularity = nx_comm.modularity(giantC, 
                                     nx_comm.label_propagation_communities(giantC))
     res.loc[i, 'Modularity'] = modularity
-    sleep(0.05)
     
-###############################################################################
+    clique_stats = CliqueStats(get_clique_count_df(G))
+    res.loc[i, 'Clique Count'] = clique_stats.clique_count
+    res.loc[i, 'Clique-Size Max'] = clique_stats.size_max
+    res.loc[i, 'Clique-Size Mode'] = clique_stats.size_mode
+    res.loc[i, 'Clique-Size Mean'] = clique_stats.size_mean
     
-# Fit a linear model
+    clique_stats = CliqueStats(get_clique_count_df(giantC))
+    res.loc[i, 'LCSG Clique Count'] = clique_stats.clique_count
+    res.loc[i, 'LCSG Clique-Size Max'] = clique_stats.size_max
+    res.loc[i, 'LCSG Clique-Size Mode'] = clique_stats.size_mode
+    res.loc[i, 'LCSG Clique-Size Mean'] = clique_stats.size_mean
 
-# SLR with modularity as predictor 
+###############################################################################
+
 X = sm.add_constant(res)
 y = list(reduced_df['Evolution'])
-model = sm.OLS(y, X.iloc[:, 2])
-est = model.fit()
-print(est.summary())
+
+# Print a pair plot of predictors with SLR R^2 > .8
+slr_r2_gt_8 = {
+    'Modularity',
+    'Clique-Size Max',
+    'Clique-Size Mean',
+    'LCSG Clique-Size Mode',
+    'LCSG Clique-Size Mean',
+    }
+sns.pairplot(X[slr_r2_gt_8], kind="reg", diag_kind="kde")
+
+# Fit a linear model
+# SLR with modularity as predictor
+for col in slr_r2_gt_8:
+    # if col != 'Modularity': continue
+    print('\n')
+    model = sm.OLS(y, X[col])
+    est = model.fit()
+    print(est.summary())
 
 ###############################################################################
 # Fit LASSO
@@ -175,6 +229,11 @@ np.mean((y - yhat)**2) # 0.007
 feature_importances = np.mean([
     tree.feature_importances_ for tree in m.estimators_
 ], axis=0)
+
+for i in np.argsort(feature_importances)[::-1]:
+    if feature_importances[i] == 0:
+        break
+    print(f'\t{res.columns[i]}: {feature_importances[i]:.3f}')
 
 ###############################################################################
 
@@ -200,7 +259,6 @@ df_list = []
 for a in tqdm(range(len(adj))):
     t = getstars(adj.iloc[a])
     df_list.append(t)
-    sleep(0.05)
     
 stars = pd.concat(df_list, axis = 1)
 stars.columns = reduced_df['Species_ID']
