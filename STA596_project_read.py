80c7f0a1811c5f4cb8e3d582ce247f1208c0dd90
Jesse Hautala <jesse.hautala@maine.edu>
2021-11-15 00:40:31 +0000

    added 'response.pickle' file and updated the 'read' script to be easier to use

--- STA596_project_read.py
+++ STA596_project_read.py
@@ -18,26 +18,68 @@ from sklearn import ensemble
 from sklearn import decomposition
 
 
-# establish constants for dealing with subsets
-sub_key = 'Taxonomy Level 2'
-sub_value = 'Bacteria_Proteobacteria'
-sub_value = 'Eukaryota_Opisthokonta'
-
-
-# NOTE: change these to work for your file paths
+# NOTE: You probably have to this to match your project location.
 data_path = 'D:/classes/STA-596/project/data/'
-src_path = f'{data_path}/species_data.output.pickle'
-X_path = f'{data_path}/{sub_key}^{sub_value}.pickle'
+
+
+# NOTE: If you want to load the original pickle file, you can just put it
+#       into the data subdirectory and uncomment this.
+# src_path = f'{data_path}/species_data.output.pickle'
+# src_df = pd.read_pickle(src_path)
+
+
+''' This function extracts and merges the predictors and response to 
+align their indices and returns them separately '''
+def get_merged(
+        selected_sub_value=None,
+        sub_key = 'Taxonomy Level 2',
+        ):
+    y = pd.read_pickle(f'{data_path}response.pickle')
+    sub_values = ['Bacteria_Proteobacteria', 'Bacteria_Terrabacteria group',
+           'Eukaryota_Stramenopiles', 'Eukaryota_Viridiplantae',
+           'Eukaryota_Opisthokonta', 'Eukaryota_Euglenozoa',
+           'Eukaryota_Heterolobosea', 'Eukaryota_Amoebozoa',
+           'Eukaryota_Alveolata', 'Eukaryota_Rhodophyta',
+           'Bacteria_FCB group', 'Archaea_Euryarchaeota',
+           'Bacteria_PVC group', 'Archaea_TACK group', 'Eukaryota_Fornicata',
+           'Bacteria_Fusobacteria', 'Bacteria_Acidobacteria',
+           'Bacteria_Aquificae', 'Archaea_DPANN group',
+           'Bacteria_Thermotogae', 'Bacteria_Spirochaetes',
+           'Bacteria_Nitrospirae', 'Bacteria_Dictyoglomi',
+           'Eukaryota_Parabasalia', 'Bacteria_Elusimicrobia',
+           'Bacteria_Synergistetes', 'Bacteria_Deferribacteres',
+           'Bacteria_Chrysiogenetes', 'Bacteria_Thermodesulfobacteria',
+           'Archaea_unclassified Archaea', 'Bacteria_unclassified Bacteria']
+    X = pd.DataFrame()
+    for sub_value in sub_values:
+        if selected_sub_value and selected_sub_value != sub_value: continue
+        sub_X_path = f'{data_path}/{sub_key}^{sub_value}.pickle'
+        sub_X = pd.read_pickle(sub_X_path)
+        X = pd.concat([X,sub_X])
+    X = X.set_index('Species_ID').fillna(0)
+    pickle.dump(X, open(f'{data_path}predictors.pickle', 'wb'))
+    merged = pd.merge(X, y, on='Species_ID')
+    X = merged.drop(columns=['Evolution'])
+    y = merged['Evolution']
+    return X, y
 
 
 # extract and merge data
-src_df = pd.read_pickle(src_path)
-sub_df = src_df[src_df[sub_key] == sub_value]
-X = pd.read_pickle(X_path).fillna(0)
-y = sub_df.set_index('Species_ID')['Evolution']
-merged = pd.merge(X, y, on='Species_ID')
-X = merged.drop(columns=['Evolution','Species_ID'])
-y = merged['Evolution']
+X, y = get_merged()
+
+
+# NOTE: pass 'Taxonomy Level 2' values to try different subsets
+# X, y = get_merged('Bacteria_Proteobacteria')
+# X, y = get_merged('Eukaryota_Opisthokonta')
+
+
+# identify sparse columns
+# NOTE: these features are poorly modeled as a DataFrame
+sparse_cols = []
+sparse_col_pattern = re.compile('(mc|d)_\d+$')
+for col in X.columns[1:]:
+    if sparse_col_pattern.match(col):
+        sparse_cols.append(col)
 
 
 # ---- pair plot
